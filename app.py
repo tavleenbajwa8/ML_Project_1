1ab1768c73240a4c5c0e9c312e5948b548d26974
tavleenbajwa8 <tavleenbajwa8@gmail.com>
2022-10-01 21:41:08 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "Starting Machine Learning Project CI CD"
 
 if __name__=="__main__":
     app.run(debug=True)
